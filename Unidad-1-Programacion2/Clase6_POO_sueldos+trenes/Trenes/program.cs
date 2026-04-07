8eb220d18f507f83f4110c81a1966acd90348396
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-04-07 20:29:48 -0300

    creando el programa

--- Unidad-1-Programacion2/Clase6_POO_sueldos+trenes/Trenes/program.cs
+++ Unidad-1-Programacion2/Clase6_POO_sueldos+trenes/Trenes/program.cs
@@ -1,10 +1,47 @@
- 
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Primero creás una formación:
+        Formacion objFormacion1 = new Formacion();
+
+        //Después creás una locomotora:
+        Locomotora objLocomotora1 = new Locomotora();
+
+        //Y luego la agregás a la formación con el método que hiciste:
+        objFormacion1.AgregarLocomotora(objLocomotora1);
+
+
+        // crear vagones de pasajeros
+        Pasajeros p1 = new Pasajeros();
+        p1.Largo = 10;
+        p1.AnchoUtil = 2;
+
+        Pasajeros p2 = new Pasajeros();
+        p2.Largo = 10;
+        p2.AnchoUtil = 3;
+
+        // agregarlos a la formación
+        objFormacion1.AgregarVagon(p1);
+        objFormacion1.AgregarVagon(p2);
+
+        //El total de pasajeros que puede transportar una formación
+        Console.WriteLine("Total de pasajeros de la formación: " + objFormacion1.TotalDePasajeros());
+
+        //2. Cuántos vagones livianos tiene una formación; un vagón es liviano si su peso máximo es menor a 2500 kg
+        Pasajeros p3 = new Pasajeros();
+        p3.Largo = 3;
+        p3.AnchoUtil = 2;
+        objFormacion1.AgregarVagon(p3);
+        Console.WriteLine($"Cantidad de vagones livianos: {objFormacion1.CantidadDeVagonesLivianos()}");
+    }
+}
 
 public class Formacion
 {
-    private List<Locomotora> _locomotoras;
-
-    private List<Vagon> _vagones;
+    private List<Locomotora> _locomotoras = new List<Locomotora>();
+    private List<Vagon> _vagones = new List<Vagon>();
     private bool _enMovimiento;
 
     public bool EnMovimiento
@@ -18,6 +55,45 @@ public class Formacion
         {
             _locomotoras.Add(locomotora);
         }
+    }
+
+    public void AgregarVagon(Vagon vagon)
+    {
+        if (!EnMovimiento)
+        {
+            _vagones.Add(vagon);
+        }
+    }
+
+    public int TotalDePasajeros()
+    {
+        int total = 0;
+
+        foreach (Vagon vagon in _vagones)
+        {
+            if (vagon is Pasajeros pasajeros)
+            {
+                total += pasajeros.CalcularCantidadDePasajeros();
+            }
+        }
+
+        return total;
+    }
+
+
+    public int CantidadDeVagonesLivianos()
+    {
+        int cantidad = 0;
+
+        foreach (Vagon vagon in _vagones)
+        {
+            if (vagon.PesoMaximo() < 2500)
+            {
+                cantidad++;
+            }
+        }
+
+        return cantidad;
     }
 }
 
@@ -78,13 +154,13 @@ public class Pasajeros : Vagon
         set { _anchoUtil = value; }
     }
 
-    private int CalcularCantidadDePasajeros()
+    public int CalcularCantidadDePasajeros()
     {
         if (AnchoUtil <= 2.5)
         {
-            return(int) Largo * 8;
+            return (int)(Largo * 8);
         }
-        return (int)Largo * 10;
+        return (int)(Largo * 10);
     }
 
     public override double PesoMaximo()
@@ -99,8 +175,8 @@ public class Carga : Vagon
 
     public double CargaMaxima
     {
-        get {return _cargaMaxima;}
-        set { _cargaMaxima = value;}
+        get { return _cargaMaxima; }
+        set { _cargaMaxima = value; }
     }
 
     public override double PesoMaximo()
