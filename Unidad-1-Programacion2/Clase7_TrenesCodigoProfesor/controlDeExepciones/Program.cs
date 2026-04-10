ab264950021fd94ead4a1f2379614d8066da231b
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-04-10 17:14:35 -0300

    Control de exepciones

---
+++ Unidad-1-Programacion2/Clase7_TrenesCodigoProfesor/controlDeExepciones/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Console.WriteLine("Ingrese su edad!");
+
+int edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Su edad es {edad}"); */
+
+/* int edad = 0;
+
+Console.WriteLine("Ingrese su edad!");
+try
+{
+    edad = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Su edad es {edad}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ha ocurrido un erro no esperado. Usted es un maravillos usuario {ex.Message}");
+} */
+
+
+
+Console.WriteLine("Ingrese su edad!");
+int edad = 0;
+try
+{
+    edad = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Su edad es {edad}");
+    //throw new DivideByZeroException();
+    double resultado = 100/ edad;
+}
+catch(FormatException ex)
+{
+    Console.WriteLine($"El formato de la edad ingresada no es correcto. Usted es un maravillos usuario {ex.Message}");
+    
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine($"Usted quiere dividir por cero y eso no se puede. 'animos!' {ex.Message}");
+    
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ha ocurrido un error no esperado. Usted es un maravillos usuario {ex.Message}");
+}
+
