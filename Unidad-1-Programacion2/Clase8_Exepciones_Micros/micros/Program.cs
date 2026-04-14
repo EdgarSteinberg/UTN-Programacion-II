3b92f4a07e3f240ccbdd1c92ca448059aded234c
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-04-14 14:15:23 -0300

    Agregando archivos programa.cs ejercicio micro

---
+++ Unidad-1-Programacion2/Clase8_Exepciones_Micros/micros/Program.cs
@@ -0,0 +1,35 @@
+﻿using Clase8_Exepciones_Micros.clases;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Micro objMicro1 = new Micro();
+
+        objMicro1.Sentados = 20;
+        objMicro1.Parados = 10;
+        objMicro1.Volumen = 150;
+
+        Persona apurado = new Apurado();
+        Persona claustrofobico = new claustrofobico();
+        Persona fiaca = new Fiaca();
+        Persona moderado = new Moderado();
+        Persona obsecuente = new Obsecuente();
+
+        Persona jefe = new Apurado();
+        obsecuente.Jefe = jefe;
+
+        Console.WriteLine($"Es jefe? {jefe.EsJefe}");
+        Console.WriteLine($"Puede subir apurado? {objMicro1.PuedeSubir(apurado)}");
+        Console.WriteLine($"Puede subir obsecuente? {objMicro1.PuedeSubir(obsecuente)}");
+
+        objMicro1.Subir(apurado);
+        objMicro1.Subir(fiaca);
+
+        Console.WriteLine("Subieron personas");
+
+        objMicro1.Bajar(fiaca);
+
+        Console.WriteLine($"Primero que subió: {objMicro1.SubioPrimero()}");
+    }
+}
--- Unidad-1-Programacion2/ejemplo-poo/obj/Debug/net10.0/ejemplo-poo.AssemblyInfo.cs
+++ Unidad-1-Programacion2/ejemplo-poo/obj/Debug/net10.0/ejemplo-poo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ae7064a9b69bec0071213a6098dc4286fea011")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b31a5e32a5d62a2c64f6180056831371b79d85f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
