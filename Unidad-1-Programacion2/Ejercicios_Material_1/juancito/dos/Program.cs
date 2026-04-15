8569f64d09cb1c13c5bc6528a4253b0f576cb813
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-04-15 20:28:18 -0300

    agregando pruebas

--- Unidad-1-Programacion2/Ejercicios_Material_1/juancito/dos/Program.cs
+++ Unidad-1-Programacion2/Ejercicios_Material_1/juancito/dos/Program.cs
@@ -1,7 +1,42 @@
-﻿
+﻿Objeto mamushkaGrande = new Objeto("Madera", 10);
+Objeto mamushkaMediana = new Objeto("Madera", 7);
+Objeto mamushkaChica = new Objeto("Madera", 4);
+
+mamushkaGrande.AgregarContenido(mamushkaMediana);
+mamushkaMediana.AgregarContenido(mamushkaChica);
+
+
+
+Objeto portafolio = new Objeto("Cuero", 5);
+
+Objeto botiquin = new Objeto("Plastico", 12);
+
+Objeto gasa = new Objeto("Textil", 1);
+Objeto aguaOxigenada = new Objeto("Liquido", 2);
+
+botiquin.AgregarContenido(gasa);
+botiquin.AgregarContenido(aguaOxigenada);
+
+Objeto bolsa = new Objeto("Cuero", 4);
+
+Objeto peine = new Objeto("Plastico", 1);
+
+bolsa.AgregarContenido(peine);
+bolsa.AgregarContenido(botiquin);
+
+mamushkaGrande.MostrarObjeto();
+portafolio.MostrarObjeto();
+botiquin.MostrarObjeto();
+bolsa.MostrarObjeto();
 
 public class Objeto
 {
+    public Objeto(string mat, int vol)
+    {
+        Material = mat;
+        Volumen = vol;
+    }
+
     private string _material;
     public string Material
     {
@@ -28,6 +63,12 @@ public class Objeto
     {
         get { return _contenedor; }
         set { _contenedor = value; }
+    }
+
+    public void AgregarContenido(Objeto obj)
+    {
+        _contenido.Add(obj);
+        obj.Contenedor = this;
     }
 
     // 🔥 Método para mostrar
