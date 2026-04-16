47f2c4d9fc5af6f899de99c03d41c71f0a95b9cc
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-04-16 14:51:54 -0300

    Pruebas Micros

---
+++ Unidad-1-Programacion2/Clase9_Static_EMicros/micros/Program.cs
@@ -0,0 +1,22 @@
+﻿using Ejercicios.clases;
+
+class Program
+{
+    static void Main()
+    {
+        Micro micro = new Micro(300,10, 5);
+
+        Fiaca fiaca = new Fiaca();   
+        micro.Subir(fiaca);
+
+        Claustrofobico claustrofobico = new Claustrofobico();
+        micro.Subir(claustrofobico);
+
+        Moderado moderado = new Moderado(5);
+        micro.Subir(moderado);
+
+        Obsecuente obsecuente = new Obsecuente(moderado);
+        micro.Subir(obsecuente);
+        moderado.EsJefe = true;
+    }
+}
