d901bbe7af78b6a4276c792e705071b76e35db3a
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-04-21 16:57:59 -0300

    agregando diagrama de clases ejercicio arbolGenialogico

--- Unidad-1-Programacion2/ejemplo-poo/obj/Debug/net10.0/ejemplo-poo.AssemblyInfo.cs
+++ Unidad-1-Programacion2/ejemplo-poo/obj/Debug/net10.0/ejemplo-poo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8569f64d09cb1c13c5bc6528a4253b0f576cb813")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df9644929d40c2484058cfd57403b5d45d543f0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
