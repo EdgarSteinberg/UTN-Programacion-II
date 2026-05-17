9cfbbcef125ce4e683c4943b2cc2df7643792037
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-05-16 21:09:08 -0300

    clase numero 3 unidad numero 2 pilas y cola

---
+++ Unidad_2_Programacion/clase3_Pilas_Cola/src/EstructuraApp/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+dotnet new sln -n ConsoleSolution
+
+dotnet new console -n ConsoleApp -o src/ConsoleApp
+dotnet sln add src/ConsoleApp/ConsoleApp.csproj
+
+dotnet new classlib -n ConsoleLibrary -o src/ConsoleLibrary
+dotnet sln add src/ConsoleLibrary/ConsoleLibrary.csproj
+
+dotnet add src/ConsoleApp/ConsoleApp.csproj reference src/ConsoleLibrary/ConsoleLibrary.csproj
+ */
+
+using EstructurasLibrary;
+
+var miPila = new MyStack<int>(); //Head -> null
+
+
+ miPila.Push(1) ; // Head -> [1 | ]-> null
+ miPila.Push(2) ; // Head -> [2 | ]-> [1 | ] => null
+ miPila.Push(3);
+
+ Console.WriteLine(miPila.Pop());
+ Console.WriteLine(miPila.Pop());
+ Console.WriteLine(miPila.Pop());
