fa9f460fca4a7d0b1b4b58bdeba6a21361e9b3d0
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-05-19 12:01:44 -0300

    pruebas ejercicio pilas editor de texto

---
+++ Unidad_2_Programacion/Pilas/3_EditorDeTexto/Program/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+Editor editor = new Editor();
+
+Accion escribir = new RedactarAccion();
+Accion imagen = new InsertarImagen();
+Accion subrayar = new SubrayarAccion();
+
+
+Console.WriteLine("== EJECUTAR ACCIONES ==");
+
+editor.EjecutarAccion(escribir);
+editor.EjecutarAccion(imagen);
+editor.EjecutarAccion(subrayar);
+
+Console.WriteLine("\n== DESHACER ==");
+
+editor.Deshacer();
+editor.Deshacer();
+
+Console.WriteLine("\n== REHACER ==");
+editor.Rehacer();
+
+editor.Rehacer();
+
+Console.WriteLine("\n== MoverImagen ==");
+Accion mover = new MoverImagenAccion("izquierda", "derecha");
+editor.EjecutarAccion(mover);
