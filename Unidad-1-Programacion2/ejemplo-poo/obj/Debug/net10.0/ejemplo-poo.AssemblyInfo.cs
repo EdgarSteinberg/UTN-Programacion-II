a038754664b8e1896a508fd0a0c96b685502ed81
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-05-20 15:37:38 -0300

    anadiendo diagrama de clases ejercicio de pilas cumpleanos

--- Unidad-1-Programacion2/ejemplo-poo/obj/Debug/net10.0/ejemplo-poo.AssemblyInfo.cs
+++ Unidad-1-Programacion2/ejemplo-poo/obj/Debug/net10.0/ejemplo-poo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da80c1a68fcb35cb074ffd8ac098b1f0e40b27ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa9f460fca4a7d0b1b4b58bdeba6a21361e9b3d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
