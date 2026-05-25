9055f956a239998b540a938758a10bdca52c511b
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-05-25 12:22:28 -0300

    diagrama de clases ejercicio colas Supermercado con pseudocodigo profesor

--- Unidad-1-Programacion2/ejemplo-poo/obj/Debug/net10.0/ejemplo-poo.AssemblyInfo.cs
+++ Unidad-1-Programacion2/ejemplo-poo/obj/Debug/net10.0/ejemplo-poo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4524983e9b2ab8c8b1d497f808933dbc2431d2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41830a253a3be162e8f9c67cdb2d8a1295b7a975")]
 [assembly: System.Reflection.AssemblyProductAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
