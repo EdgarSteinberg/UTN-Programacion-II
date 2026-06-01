a3f39c2fc3dbaf4c2410120794239b45b8c2d43a
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-06-01 13:10:27 -0300

    actualizacion diagrama elctrodomesticos

--- Unidad-1-Programacion2/ejemplo-poo/obj/Debug/net10.0/ejemplo-poo.AssemblyInfo.cs
+++ Unidad-1-Programacion2/ejemplo-poo/obj/Debug/net10.0/ejemplo-poo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2315e821da0b6dc59be9a626349cb9f30ebebc3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38411b3e1b2732b08d5e7e8874fbf15e53fe211b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
