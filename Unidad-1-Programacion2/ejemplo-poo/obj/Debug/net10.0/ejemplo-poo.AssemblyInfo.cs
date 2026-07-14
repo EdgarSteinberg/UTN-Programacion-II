cf1c713d6875456142724655a8f6390f6642c44d
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-07-14 10:35:44 -0300

    Estructura de datos Colas tipo FIFO

--- Unidad-1-Programacion2/ejemplo-poo/obj/Debug/net10.0/ejemplo-poo.AssemblyInfo.cs
+++ Unidad-1-Programacion2/ejemplo-poo/obj/Debug/net10.0/ejemplo-poo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bd95d5be24c5f49e39a55c0d11d14efcb6de836")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0713862d22f2b87c183917f402f1277b73bb8cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ejemplo-poo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
